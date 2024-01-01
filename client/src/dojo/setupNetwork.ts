7d57feb529447ae8fef750c7fc248de0de265df2
Zack Ames <zames6584@gmail.com>
2024-01-01 12:08:28 -0500

    vercel fix attempt

--- client/src/dojo/setupNetwork.ts
+++ client/src/dojo/setupNetwork.ts
@@ -2,7 +2,7 @@ import { defineContractComponents } from "./contractComponents";
 import { world } from "./world";
 import { DojoProvider } from "@dojoengine/core";
 import { Account, num } from "starknet";
-import manifest from "../../../dojo-project/target/release/manifest.json";
+import manifest from "../manifest.json";
 import * as torii from "@dojoengine/torii-client";
 
 export type SetupNetworkResult = Awaited<ReturnType<typeof setupNetwork>>;
--- client/src/scenes/Scene.tsx
+++ client/src/scenes/Scene.tsx
@@ -4,7 +4,6 @@ import { FC } from "react";
 import Secret from "../components/Secret";
 import Button from "../components/Button";
 import Challenge from "../components/Challenge";
-import Games from "../components/Games";
 
 interface SceneProps {
     setup: {
@@ -31,8 +30,6 @@ const Scene: FC<SceneProps> = ({setup: {components, systemCalls}, account, game_
 
             <Button position={[-5,5,0]} onClick={() => create_piece(signer, 1)} label="create piece"/>
             <Challenge position={[-5,10,0]} list={account.list} challenge={challenge} signer = {signer}/>
-
-            <Games game_ids={game_ids} position={[-10,0,0]} components={components} signer={signer} take_turn={take_turn} />
 
             <Button scale={30} position = {[0,10,-50]} onClick={() => spawn(signer)} label = "spawn"/> 
             <Secret position={[10,5,-10]} onClick = {() => set_secret(signer, 200)} components={components} signer={signer} />
