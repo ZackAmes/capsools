4ab4bc4a4abe4afaacb66daf7229126a133d7049
Zack Ames <zames6584@gmail.com>
2024-01-01 20:25:52 -0500

    get moves

--- client/src/components/Board.tsx
+++ client/src/components/Board.tsx
@@ -12,6 +12,7 @@ import { RigidBody } from "@react-three/rapier";
 import Piece from "./Piece";
 import update_position from "../utils/update_position";
 import { Ref , useState} from "react";
+import get_moves from "../utils/get_moves";
 interface BoardProps {
     position: [number,number,number]
     game_id:number
@@ -39,8 +40,11 @@ const Board: FC<BoardProps> = ({position, game_id, piece_ids, components, signer
     let [cur_x, set_x] = useState(0);
     let [cur_y, set_y] = useState(0);
     let [cur_is_piece, set_is_piece] = useState(false);
+    let initial_moves:[number, number][] = [[0,0]]
+    let [cur_moves, set_moves] = useState(initial_moves);
+    
 
-    let set_cur = (x:number, y:number, clicked_is_piece: boolean) => {
+    let set_cur = (x:number, y:number, clicked_is_piece: boolean, id: number = 0) => {
         if(!cur_is_piece){
             set_x(x);
             set_y(y);
@@ -48,6 +52,16 @@ const Board: FC<BoardProps> = ({position, game_id, piece_ids, components, signer
                 set_is_piece(true);
             }
         }
+        set_x(x);
+        set_y(y);
+        if(clicked_is_piece){
+            set_moves(get_moves(id))
+        }
+        else {
+            set_moves([]);
+        }
+        set_is_piece(clicked_is_piece)
+
         //else move, or attack
     }
 
@@ -59,7 +73,7 @@ const Board: FC<BoardProps> = ({position, game_id, piece_ids, components, signer
         let piece = getComponentValue(components.Piece, getEntityIdFromKeys([BigInt(piece_id)]) as Entity);
         if(piece){
             let piece_position: [number, number, number] = [piece.data.position.x, 2.1, piece.data.position.y];
-            piece_positions.push({x:piece.data.position.x, y:piece.data.position.y})
+            piece_positions.push({x:piece.data.position.x, y:piece.data.position.y, type: piece.data.piece_type, id: piece.id})
             return (<Piece key={piece.id} position={piece_position} type={piece.data.piece_type}/>)
         }    
     })
@@ -77,17 +91,28 @@ const Board: FC<BoardProps> = ({position, game_id, piece_ids, components, signer
         for(let i=0; i< piece_positions.length; i++) {
             let piece_position = piece_positions[i];
             if(x==piece_position.x && y==piece_position.y){
-                clicked = () => set_cur(x,y,true);
+                if(cur_x == x && cur_y == y && cur_is_piece == true) {
+                    clicked = () => set_cur(0,0,false);
+                }
+                clicked = () => set_cur(x,y,true, piece_position.type);
             }
         }
         let color = x%2==y%2 ? "black" : "white"
+        if(cur_x == x && cur_y == y && cur_is_piece == true) {
+            color = "blue"
+        }
+        for(let i=0; i<cur_moves.length; i++) {
+            if(cur_x + cur_moves[i][0] == x && cur_y + cur_moves[i][1] == y) {
+                color = "green"
+            }
+        }
         let tempPosition: [number, number, number] = [x, 0, y];
         return (<Square key={index} ref={ref} 
                         position={tempPosition} color= {color} 
                         onClick={clicked}/>);
     })
 
-
+    
 
 
 
